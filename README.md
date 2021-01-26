9661dfea0f1b498ec452963faa2aecbcc46baa1a
itchecosta <itchecosta@gmail.com>
2021-01-25 21:42:57 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center"> NASA Image </h1>
+
+## Sobre :memo:
+
+Exibir imagens no terminal da NASA (APOD)
+
+## Referência :arrow_forward:
+
+[Danilo Filitto](https://www.youtube.com/watch?v=s6O-qXZiIrM&list=PLfvOpw8k80WqZsXphHeeSVNKr6EovKir-&index=28)
---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const terminalImage = require("terminal-image");
+const got = require('got');
+
+async function showImage(img){
+    const body = await got(img).buffer();
+    //console.log(body);
+    console.log(await terminalImage.buffer(body));
+}
+
+async function getAPOD(){
+    const date = '&date=2021-01-24';
+    const dados = await got('https://api.nasa.gov/planetary/apod?api_key='+process.env.API_KEY+date, {responseType: 'json', resolveBodyOnly: true});
+    //console.log(dados.url);
+    showImage(dados.url);
+}
+// pegar os dados na nasa (APOD)
+getAPOD();
